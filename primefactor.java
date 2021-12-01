50a5bcda0b4ade1262aea7d03eba24857e044096
shareef9 <95331106+shareef9@users.noreply.github.com>
2021-12-01 10:16:31 +0530

    Update primefactor.java

--- primefactor.java
+++ primefactor.java
@@ -3,30 +3,18 @@ public class primefactor {
 
 		static long MaxPrime(long num) {
 		    long CurrMaxPrime = -1;
-
-		    //If num is divisible by 2, store CurrMaxPrime
-		    //as 2. keep on dividing num until it is not 
-		    //divisible by 2. After each division, update 
-		    //num as num/2.
 		    if(num % 2 == 0) {
 		      CurrMaxPrime = 2;
 		      while(num % 2 == 0){
 		        num = num/2;
 		      }
 		    }
-		    
-		    //At this point, num must be odd. Starting with 
-		    //3 to square root of num , if divisible, divide 
-		    //and update num, and update CurrMaxPrime
 		    for(long i = 3; i <= Math.sqrt(num); i += 2) {
 		      while(num % i == 0) {
 		        CurrMaxPrime = i;
 		        num = num/i;
 		      }
 		    }
-
-		    //if the modified num is greater than 2, 
-		    //update the CurrMaxPrime with num 
 		    if (num > 2) 
 		      CurrMaxPrime = num;
 
